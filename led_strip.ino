40ab9f9545be2b5a9e2358adad052c9a91d55e25
J.D. Zamfirescu <zamfire@gmail.com>
2014-04-27 12:16:24 -0700

    Update led_strip.ino

--- led_strip.ino
+++ led_strip.ino
@@ -9,13 +9,13 @@
 int nLEDs = 50;
 
 // Chose 2 pins for output; can be any valid output pins:
-int dataPin  = 2;
-int clockPin = 3;
+// int dataPin  = 2;
+// int clockPin = 3;
 
 // First parameter is the number of LEDs in the strand.  The LED strips
 // are 32 LEDs per meter but you can extend or cut the strip.  Next two
 // parameters are SPI data and clock pins:
-LPD8806 strip = LPD8806(nLEDs, dataPin, clockPin);
+// LPD8806 strip = LPD8806(nLEDs, dataPin, clockPin);
 
 // You can optionally use hardware SPI for faster writes, just leave out
 // the data and clock pin parameters.  But this does limit use to very
@@ -24,7 +24,7 @@ LPD8806 strip = LPD8806(nLEDs, dataPin, clockPin);
 // clock = pin 52.  For 32u4 Breakout Board+ and Teensy, data = pin B2,
 // clock = pin B1.  For Leonardo, this can ONLY be done on the ICSP pins.
 
-//LPD8806 strip = LPD8806(nLEDs);
+LPD8806 strip = LPD8806(nLEDs);
 
 void setup() {
   // Start up the LED strip
